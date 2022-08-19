746a9d3b45127c25e53564f4ad3b8223d87dabac
kimgoesflying <yelizarova.ae@gmail.com>
2022-08-19 20:03:06 +0300

    install django REST framework

--- FSTR/settings.py
+++ FSTR/settings.py
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'phonenumber_field',
-
     'mountain_pass',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -54,6 +54,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 ROOT_URLCONF = 'FSTR.urls'
 
--- mountain_pass/views.py
+++ mountain_pass/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import permissions
 
-# Create your views here.
+from .serializers import *
+from .models import *
+
+
+class TouristViewset(viewsets.ModelViewSet):
+    queryset = Tourist.objects.all()
+    serializer_class = TouristSerializer
+
+    def list(self, request, format=None):
+        return Response([])
