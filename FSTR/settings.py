4149801ebde95a8fea8bf4d7b7bcf439a1967e38
kimgoesflying <yelizarova.ae@gmail.com>
2022-08-19 18:10:04 +0300

    configure postgres db

--- FSTR/settings.py
+++ FSTR/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&!nb+3qo6+%ew=sl)&9xcii&@=j2ozw(4bt2%0dfb7t!-yzrwj'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,6 +83,17 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': str(os.getenv('FSTR_DB_NAME')),
+        'USER': str(os.getenv('FSTR_DB_LOGIN')),
+        'PASSWORD': str(os.getenv('FSTR_DB_PASS')),
+        'HOST': str(os.getenv('FSTR_DB_HOST')),
+        'PORT': str(os.getenv('FSTR_DB_PORT')),
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -103,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
