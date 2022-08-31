b675decd55473aa57323acdf2157c805c1cac1ec
kimgoesflying <yelizarova.ae@gmail.com>
2022-08-31 16:16:33 +0300

    heroku deploy

--- FSTR/settings.py
+++ FSTR/settings.py
@@ -141,11 +141,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
