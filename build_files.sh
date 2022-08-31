1f4edce18fffc9a65519864b4309e4abf213ec2b
kimgoesflying <yelizarova.ae@gmail.com>
2022-08-31 13:09:15 +0300

    edit settings for vercel deploy

--- build_files.sh
+++ build_files.sh
@@ -4,10 +4,5 @@
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
-echo "Migrating database..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-
-
 echo "Collecting static files..."
 python3.9 manage.py collectstatic --noinput
